7c892fd032cf183f0879cbada8bf5249be3bed35
marlontancredo <marlon_tancredo@hotmail.com>
2023-01-11 19:49:45 +0000

    Changing project

--- portfolio/saved.js
+++ portfolio/saved.js
@@ -1,9 +1,56 @@
-const savedItems = JSON.parse(sessionStorage.getItem("saved-items"));
-console.log(savedItems[0]);
+const dataList = [];
 
-const savedDiv = document.getElementById("saved");
-const marlonImage = document.createElement("img");
+const sessionStorageData = "saved-data";
 
-savedDiv.append(marlonImage);
+const getSavedData = (value) => {
+  const mySavedData = JSON.parse(sessionStorage.getItem(`${value}`));
+  return mySavedData;
+};
 
-marlonImage.src = savedItems[0];
+const savedData = getSavedData(sessionStorageData);
+
+if (savedData !== null) {
+  for (let i = 0; i < savedData.length; i++) {
+    dataList.push(savedData[i]);
+  }
+
+  const container = document.getElementById("container");
+
+  for (let i = 0; i < savedData.length; i++) {
+    if (savedData[i].id === "header-image") {
+      const img = document.createElement("img");
+      img.src = savedData[i].value;
+      container.append(img);
+    }
+    if (savedData[i].id === "about-me-paragraph") {
+      const paragraph = document.createElement("p");
+      paragraph.innerHTML = savedData[i].value;
+      container.append(paragraph);
+    }
+    if (savedData[i].id === "my-photos-1") {
+      const img = document.createElement("img");
+      img.src = savedData[i].value;
+      container.append(img);
+    }
+    if (savedData[i].id === "my-photos-2") {
+      const img = document.createElement("img");
+      img.src = savedData[i].value;
+      container.append(img);
+    }
+    if (savedData[i].id === "my-photos-3") {
+      const img = document.createElement("img");
+      img.src = savedData[i].value;
+      container.append(img);
+    }
+    if (savedData[i].id === "my-photos-4") {
+      const img = document.createElement("img");
+      img.src = savedData[i].value;
+      container.append(img);
+    }
+    if (savedData[i].id === "my-photos-5") {
+      const img = document.createElement("img");
+      img.src = savedData[i].value;
+      container.append(img);
+    }
+  }
+}
